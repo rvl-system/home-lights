78af5eb93e59df63a9e5ddfadef0aecc83962dc7
Bryan Hughes <bryan@nebri.us>
2019-04-20 23:36:14 -0700

    Set up web interface scaffolding

---
+++ dist/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+Copyright (c) Bryan Hughes <bryan@nebri.us>
+
+This file is part of Home Lights.
+
+Home Lights is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Home Lights is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Home Lights.  If not, see <http://www.gnu.org/licenses/>.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
+const fs_1 = require("fs");
+const rvl_node_1 = require("rvl-node");
+const express = require("express");
+const handlebars_1 = require("handlebars");
+const WEB_SERVER_PORT = 80;
+const RAVER_LIGHTS_INTERFACE = 'wifi0';
+const indexTemplate = handlebars_1.compile(fs_1.readFileSync(path_1.join(__dirname, '..', 'templates', 'index.handlebars')).toString());
+const rvl = new rvl_node_1.RVL({
+    networkInterface: RAVER_LIGHTS_INTERFACE,
+    port: 4978,
+    mode: 'controller',
+    logLevel: 'debug'
+});
+rvl.on('initialized', () => {
+    const app = express();
+    app.use(express.static(path_1.join(__dirname, '..', 'public')));
+    app.get('/', (req, res) => {
+        res.send(indexTemplate({}));
+    });
+    app.listen(WEB_SERVER_PORT, () => {
+        rvl.start();
+        console.log(`Example app listening on port ${WEB_SERVER_PORT}!`);
+    });
+});
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,51 @@
 /*
 Copyright (c) Bryan Hughes <bryan@nebri.us>
 
-This file is part of Raver Lights Messaging.
+This file is part of Home Lights.
 
-Raver Lights Messaging is free software: you can redistribute it and/or modify
+Home Lights is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Raver Lights Messaging is distributed in the hope that it will be useful,
+Home Lights is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Raver Lights Messaging.  If not, see <http://www.gnu.org/licenses/>.
+along with Home Lights.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { join } from 'path';
+import { readFileSync } from 'fs';
 import { RVL } from 'rvl-node';
 import * as express from 'express';
+import { compile } from 'handlebars';
 
-const app = express();
-const port = 80;
+const WEB_SERVER_PORT = 80;
+const RAVER_LIGHTS_INTERFACE = 'wifi0';
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const indexTemplate = compile(readFileSync(join(__dirname, '..', 'templates', 'index.handlebars')).toString());
 
 const rvl = new RVL({
-  networkInterface: 'wlan0',
+  networkInterface: RAVER_LIGHTS_INTERFACE,
   port: 4978,
-  mode: "controller",
-  logLevel: "debug"
+  mode: 'controller',
+  logLevel: 'debug'
 });
 
-rvl.start();
+rvl.on('initialized', () => {
+  const app = express();
+
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  app.get('/', (req, res) => {
+    res.send(indexTemplate({}));
+  });
+
+  app.listen(WEB_SERVER_PORT, () => {
+    console.log(`Home Lights server running on port ${WEB_SERVER_PORT}!`);
+    rvl.start();
+  });
+});
