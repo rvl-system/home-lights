561bc158c2338d47c141bf0f717b6e6cabf686c4
Bryan Hughes <bryan@nebri.us>
2020-08-27 20:20:24 -0700

    Got server working

--- server/dist/sqlite.js
+++ server/dist/sqlite.js
@@ -17,51 +17,15 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Home Lights.  If not, see <http://www.gnu.org/licenses/>.
 */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbGet = exports.dbAll = exports.dbRun = exports.init = void 0;
-var sqlite3_1 = require("sqlite3");
-var util_1 = require("./util");
-var db;
+const sqlite3_1 = require("sqlite3");
+const util_1 = require("./util");
+let db;
 function init(dbPath) {
-    var sqlite3 = sqlite3_1.verbose();
-    return new Promise(function (resolve, reject) {
-        db = new sqlite3.Database(dbPath, function (err) {
+    const sqlite3 = sqlite3_1.verbose();
+    return new Promise((resolve, reject) => {
+        db = new sqlite3.Database(dbPath, (err) => {
             if (err) {
                 reject(err);
             }
@@ -75,22 +39,18 @@ exports.init = init;
 /**
  * Runs all sqlite queries queries other than SELECT
  */
-function dbRun(query, parameters) {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            if (!db) {
-                throw util_1.createInternalError("dbRun called before database initialized");
+async function dbRun(query, parameters) {
+    if (!db) {
+        throw util_1.createInternalError(`dbRun called before database initialized`);
+    }
+    return new Promise((resolve, reject) => {
+        db.run(query, parameters || [], function (err) {
+            if (err) {
+                reject(err);
             }
-            return [2 /*return*/, new Promise(function (resolve, reject) {
-                    db.run(query, parameters || [], function (err) {
-                        if (err) {
-                            reject(err);
-                        }
-                        else {
-                            resolve(this.lastID);
-                        }
-                    });
-                })];
+            else {
+                resolve(this.lastID);
+            }
         });
     });
 }
@@ -98,23 +58,18 @@ exports.dbRun = dbRun;
 /**
  * SELECT all rows from the database
  */
-function dbAll(query, parameters) {
-    if (parameters === void 0) { parameters = []; }
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            if (!db) {
-                throw util_1.createInternalError("dbAll called before database initialized");
+async function dbAll(query, parameters = []) {
+    if (!db) {
+        throw util_1.createInternalError(`dbAll called before database initialized`);
+    }
+    return new Promise((resolve, reject) => {
+        db.all(query, parameters, (err, results) => {
+            if (err) {
+                reject(err);
             }
-            return [2 /*return*/, new Promise(function (resolve, reject) {
-                    db.all(query, parameters, function (err, results) {
-                        if (err) {
-                            reject(err);
-                        }
-                        else {
-                            resolve(results);
-                        }
-                    });
-                })];
+            else {
+                resolve(results);
+            }
         });
     });
 }
@@ -122,23 +77,18 @@ exports.dbAll = dbAll;
 /**
  * Used to SELECT the first row out of the database
  */
-function dbGet(query, parameters) {
-    if (parameters === void 0) { parameters = []; }
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            if (!db) {
-                throw util_1.createInternalError("dbGet called before database initialized");
+async function dbGet(query, parameters = []) {
+    if (!db) {
+        throw util_1.createInternalError(`dbGet called before database initialized`);
+    }
+    return new Promise((resolve, reject) => {
+        db.get(query, parameters, (err, result) => {
+            if (err) {
+                reject(err);
             }
-            return [2 /*return*/, new Promise(function (resolve, reject) {
-                    db.get(query, parameters, function (err, result) {
-                        if (err) {
-                            reject(err);
-                        }
-                        else {
-                            resolve(result);
-                        }
-                    });
-                })];
+            else {
+                resolve(result);
+            }
         });
     });
 }
