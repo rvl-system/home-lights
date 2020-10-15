f160c7a42311f9616d53b24e7081fa0560ad05e4
Bryan Hughes <bryan@nebri.us>
2020-10-14 22:01:14 -0700

    Normalized reducer and listener filenames

--- client/src/reducers/reducers.ts
+++ client/src/reducers/reducers.ts
@@ -17,6 +17,6 @@ You should have received a copy of the GNU General Public License
 along with Home Lights.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import './zones';
-import './lights';
-import './selectedTab';
+import './zonesReducer';
+import './lightsReducer';
+import './selectedTabReducer';
