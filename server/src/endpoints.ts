5c7039053e167f46274b633ef923812a42dcbdfc
Bryan Hughes <bryan@nebri.us>
2020-12-20 15:22:31 -0800

    Added updated icons

--- server/src/endpoints.ts
+++ server/src/endpoints.ts
@@ -42,7 +42,7 @@ export function init(): Promise<void> {
     initLights(app);
     initState(app);
 
-    app.listen(port, (err, address) => {
+    app.listen(port, '0.0.0.0', (err, address) => {
       if (err) {
         app.log.error(err);
         process.exit(1);
