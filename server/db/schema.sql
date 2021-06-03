9d8be2419dddcb32a518648d0360e72735e28e7a
Bryan Hughes <bryan@nebri.us>
2021-06-03 13:03:12 -0700

    Updated schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -45,3 +45,10 @@ CREATE TABLE IF NOT EXISTS "migrations" (
 CREATE TABLE IF NOT EXISTS "settings" (
   theme TEXT NOT NULL
 );
+CREATE TABLE "system_state" (
+  zone_id INTEGER NOT NULL UNIQUE,
+  power INTEGER DEFAULT 0,
+  current_scene_id INTEGER,
+  FOREIGN KEY (zone_id) REFERENCES zones(id)
+  FOREIGN KEY (current_scene_id) REFERENCES scenes(id)
+);
